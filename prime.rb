62f5bfcaa1a345a70699b76f3c0ecc7bbd30c775
Kyle Graden <kylegraden3@gmail.com>
2019-03-09 02:09:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,24 @@
+##def prime?(n)
+##    if n <= 1
+##        false
+##    elsif n == 2
+##        true
+##    else
+##        (2..n/2).none? { |i| n % i == 0}
+##    end
+##end
+
+
 def prime?(n)
     if n <= 1
         false
     elsif n == 2
         true
     else
-        (2..n/2).none? { |i| n % i == 0}
+        for i in 2..n-1 
+          if n % i == 0}
+            false
+          else
+            true
     end
 end
